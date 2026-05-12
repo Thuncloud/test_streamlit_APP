29a8f0fd15f5ae85e9c46e7d8d4e6f23d71b9220
Thuncloud <130273867+Thuncloud@users.noreply.github.com>
2026-05-12 15:36:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ st.write(f"你好，{name1}！")
 
 number = st.text_input("請輸入數字：", value = 0)
 if number != 0:
-    number = number*10
+    number = number * 10
     st.write(f"你輸入的數字10被為{number}!")
 
 # 按鈕
