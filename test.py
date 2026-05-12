ab9f682d644e8d4276cc0afbe213965f9486d856
Thuncloud <130273867+Thuncloud@users.noreply.github.com>
2026-05-12 15:25:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ st.title("Hello, Streamlit!")
 st.write("這是一個簡單的範例，歡迎來到 Streamlit 的世界！")
 
 # 文字輸入框
-name = st.text_input("請輸入你的名字：", value="你的名字")
-st.write(f"你好，{name}！")
+name1 = st.text_input("請輸入你的名字：", value="你的名字")
+st.write(f"你好，{name1}！")
 # 文字輸入框
-name = st.text_input("請輸入你的名字：", value="你的名字")
-st.write(f"你好，{name}！")
+name2 = st.text_input("請輸入你的名字：", value="你的名字")
+st.write(f"你好，{name2}！")
 # 按鈕
 if st.button("點擊我"):
     st.write("你剛剛點擊了按鈕！")
