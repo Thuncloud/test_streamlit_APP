a20b7073fbcca5496aa2b733dfb6e1fdf5533a46
Thuncloud <130273867+Thuncloud@users.noreply.github.com>
2026-05-12 16:14:33 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ st.write("這是一個簡單的範例，歡迎來到 Streamlit 的世界！")
 name1 = st.text_input("請輸入你的名字：", value="你的名字")
 st.write(f"你好，{name1}！")
 
-number1 = st.number_input("請輸入數字：")
-number2 = st.number_input("請輸入數字：")
+number1 = st.number_input("請輸入數字：", key = num_1)
+number2 = st.number_input("請輸入數字：", key = num_2)
 
 if number is not None:
     st.write(f"你輸入了 {number1}")
