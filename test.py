2602af3e01a6583708ad75801771990c1427c760
Thuncloud <130273867+Thuncloud@users.noreply.github.com>
2026-05-12 15:51:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ st.write("這是一個簡單的範例，歡迎來到 Streamlit 的世界！")
 name1 = st.text_input("請輸入你的名字：", value="你的名字")
 st.write(f"你好，{name1}！")
 
-number = st.text_input("請輸入數字：", value = 0)
+number = st.number_input("請輸入數字：", value = 0)
 if number != 0:
-    number = number + 10
-    st.write(f"你輸入的數字+10被為{number}!")
+    result = number + 10
+    st.write(f"你輸入的數字+10被為{result}!")
 
 # 按鈕
 if st.button("點擊我"):
