28111443777b974c02444084d6cbf6d3465a2b10
Thuncloud <130273867+Thuncloud@users.noreply.github.com>
2026-05-12 15:32:50 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,10 @@ st.write("這是一個簡單的範例，歡迎來到 Streamlit 的世界！")
 # 文字輸入框
 name1 = st.text_input("請輸入你的名字：", value="你的名字")
 st.write(f"你好，{name1}！")
+
+number = st.ttext_input("請輸入數字：")
+st.write(f"你輸入的數字10被為{number*10}!")
+
 # 按鈕
 if st.button("點擊我"):
     st.write("你剛剛點擊了按鈕！")
