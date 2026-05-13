1a92a2a690dce7867179a78c39c973b22bf0160f
Thuncloud <130273867+Thuncloud@users.noreply.github.com>
2026-05-13 10:35:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -32,6 +32,19 @@ if uploaded_file is not None:
                 st.write("灰階處理")
                 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                 st.image(gray_image)
+                
+            case "二值化處理":
+                st.write("二值化處理")
+                # 限定範圍在 0.0 到 1.0 之間 (適合權重或機率調整)
+                thresh = st.slider(
+                    "請選擇透明度：", 
+                    min_value=0, 
+                    max_value=255, 
+                    value=127, 
+                    step=1
+                )
+                ret, output = cv2.threshold(image, thresh, 255, cv2.THRESH_BINARY)
+                st.image(output)
 
 
             
